d8294311732363be6c50b83a9f63c2bb72d144a9
Mike Hayes <mkhayes@gmail.com>
2013-03-03 00:39:50 -0500

    Finished RFView JSON deserialization.

---
+++ src/edu/oswego/csc480_hci521_2013/h2owrapper/json/objects/ConfusionMatrix.java
@@ -0,0 +1,31 @@
+package edu.oswego.csc480_hci521_2013.h2owrapper.json.objects;
+import java.util.List;
+
+public class ConfusionMatrix
+{
+
+/* Example JSON
+  "confusion_matrix" : {
+    "type" : "full scoring",
+    "classification_error" : 0.0,
+    "rows_skipped" : 0,
+    "rows" : 150,
+    "header" : [ "Iris-setosa", "Iris-versicolor", "Iris-virginica" ],
+    "scores" : [ [ 50, 0, 0 ], [ 0, 50, 0 ], [ 0, 0, 50 ] ],
+    "used_trees" : 50
+  },
+ */
+    private String type;
+    private float classification_error;
+    private int rows_skipped;
+    private int rows;
+    private List<String> header;
+    private List<List<Integer>> scores;
+    private int used_trees;
+
+    @Override
+    public String toString()
+    {
+        return "ConfusionMatrix{" + "type=" + type + ", classification_erro=" + classification_error+ ", rows_skipped=" + rows_skipped + ", rows=" + rows + ", header=" + header+ ", scores=" + scores+ ", used_trees=" + used_trees + '}';
+    }
+}
---
+++ src/edu/oswego/csc480_hci521_2013/h2owrapper/json/objects/RFViewTreeProperties.java
@@ -0,0 +1,42 @@
+package edu.oswego.csc480_hci521_2013.h2owrapper.json.objects;
+import java.util.Map;
+
+public class RFViewTreeProperties {
+	private int number_built;
+	private Map<String, Float> depth;
+	private Map<String, Float> leaves;
+
+	public int getNumberBuilt(){
+		return number_built;
+	}
+
+	public float getDepthMin(){
+		return depth.get("min");
+	}
+
+	public float getDepthMean(){
+		return depth.get("mean");
+	}
+
+	public float getDepthMax(){
+		return depth.get("max");
+	}
+
+	public float getLeavesMin(){
+		return leaves.get("min");
+	}
+
+	public float getLeavesMean(){
+		return leaves.get("mean");
+	}
+
+	public float getLeavesMax(){
+		return leaves.get("max");
+	}
+
+	@Override
+	public String toString(){
+		return "trees{ number_built=" + number_built + ", depth_min=" + getDepthMin() + ", depth_mean=" + getDepthMean() + ", depth_max=" + getDepthMax() + ", leaves_min=" + getLeavesMin() + ", leaves_mean=" + getLeavesMean() + ", leaves_max=" + getLeavesMax() + "}";
+	}
+
+}
