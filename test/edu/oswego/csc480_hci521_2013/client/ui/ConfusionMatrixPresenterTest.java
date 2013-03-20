c8b99860ecd7c6828f70296ab57d4092590ddcd2
Kevin Winahradsky <kevin.winahradsky@gmail.com>
2013-03-19 20:53:07 -0400

    fixed rename in test

--- test/edu/oswego/csc480_hci521_2013/client/ui/ConfusionMatrixPresenterTest.java
+++ test/edu/oswego/csc480_hci521_2013/client/ui/ConfusionMatrixPresenterTest.java
@@ -1,15 +1,15 @@
 // Copyright 2013 State University of New York at Oswego
 //
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 // http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package edu.oswego.csc480_hci521_2013.client.ui;
@@ -38,7 +38,7 @@ import org.mockito.MockitoAnnotations;
  * @author Patrick
  */
 public class ConfusionMatrixPresenterTest {
-    
+
     @Mock
     private RFView randomForest;
     @Mock
@@ -53,18 +53,18 @@ public class ConfusionMatrixPresenterTest {
     private MinMeanMax treeFeaturesLeaves;
     @Mock
     private MinMeanMax treeFeaturesDepth;
-    
+
     public ConfusionMatrixPresenterTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -74,92 +74,92 @@ public class ConfusionMatrixPresenterTest {
         when(tree.getLeaves()).thenReturn(treeFeaturesLeaves);
         when(tree.getDepth()).thenReturn(treeFeaturesDepth);
     }
-    
+
     @After
     public void tearDown() {
     }
     @Test
     public void test_total_trees() {
         final int treeCount = 1000;
-        
+
         when(randomForest.getNtree()).thenReturn(treeCount);
         when(status.isPoll()).thenReturn(true);
-        
+
         ConfusionMatrixPresenterImpl.updateView(matrixView, randomForest);
-        verify(matrixView).setNtree("1000");               
+        verify(matrixView).setNtree("1000");
     }
-    
+
     @Test
     public void test_no_progress() {
         final int treeCount = 0;
-        
+
         when(randomForest.getNtree()).thenReturn(treeCount);
         when(status.isPoll()).thenReturn(true);
-        
+
         ConfusionMatrixPresenterImpl.updateView(matrixView, randomForest);
-        verify(matrixView).setProgress("0");               
+        verify(matrixView).setProgress("0");
     }
-    
+
     @Test
     public void test_some_progress() {
         final int treesBuilt = 50;
         final int treeCount = 100;
-        
+
         when(randomForest.getNtree()).thenReturn(treeCount);
         when(status.isPoll()).thenReturn(true);
         when(tree.getNumberBuilt()).thenReturn(treesBuilt);
-        
+
         ConfusionMatrixPresenterImpl.updateView(matrixView, randomForest);
-        verify(matrixView).setProgress("50.0");               
+        verify(matrixView).setProgress("50.0");
     }
-    
+
     @Test
     public void test_total_progress() {
         final int treesBuilt = 100;
         final int treeCount = 100;
-        
+
         when(randomForest.getNtree()).thenReturn(treeCount);
         when(status.isPoll()).thenReturn(true);
         when(tree.getNumberBuilt()).thenReturn(treesBuilt);
-        
+
         ConfusionMatrixPresenterImpl.updateView(matrixView, randomForest);
-        verify(matrixView).setProgress("100.0");               
+        verify(matrixView).setProgress("100.0");
     }
-    
+
     @Test
     public void test_trees() {
         final int treesBuilt = 100;
         final int treeCount = 100;
-        
+
         when(randomForest.getNtree()).thenReturn(treeCount);
         when(status.isPoll()).thenReturn(true);
         when(tree.getNumberBuilt()).thenReturn(treesBuilt);
-        
+
         ConfusionMatrixPresenterImpl.updateView(matrixView, randomForest);
-        verify(matrixView).setNtree("100");               
+        verify(matrixView).setNtree("100");
     }
-    
+
     @Test
     public void test_mtry() {
         final int mtry = 7;
-        
+
         when(randomForest.getMtry()).thenReturn(mtry);
-        
+
         ConfusionMatrixPresenterImpl.updateView(matrixView, randomForest);
-        verify(matrixView).setMtry("7");               
+        verify(matrixView).setMtry("7");
     }
-    
+
     @Test
     public void test_matrix_type() {
         final String matrixType = "unknown";
-        
+
         when(randomForest.getConfusionMatrix()).thenReturn(matrix);
         when(matrix.getType()).thenReturn(matrixType);
-        
+
         ConfusionMatrixPresenterImpl.updateView(matrixView, randomForest);
         verify(matrixView).setMatrixType(matrixType);
     }
-    
+
     @Test
     public void test_matrix() {
         List<List<Integer>> scores = new ArrayList<List<Integer>>(Arrays.asList(
@@ -167,12 +167,12 @@ public class ConfusionMatrixPresenterTest {
                 new ArrayList<Integer>(Arrays.asList(0,1,0)),
                 new ArrayList<Integer>(Arrays.asList(0,0,1))
                 ));
-        
+
         when(matrix.getScores()).thenReturn(scores);
         when(randomForest.getConfusionMatrix()).thenReturn(matrix);
-        
-        ConfusionMatrixPresenterImpl.UpdateView(matrixView, randomForest);
-        //verify(matrixView).setMatrixType(matrixType);        
+
+        ConfusionMatrixPresenterImpl.updateView(matrixView, randomForest);
+        //verify(matrixView).setMatrixType(matrixType);
     }
-            
+
 }
