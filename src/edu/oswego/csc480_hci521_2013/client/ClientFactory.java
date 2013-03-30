dd4645b96fe873b4994dc1731919becbd9ae86b7
Patrick Toohey <ptoohey@oswego.edu>
2013-03-29 21:52:02 -0400

    clear clientfactory

--- src/edu/oswego/csc480_hci521_2013/client/ClientFactory.java
+++ src/edu/oswego/csc480_hci521_2013/client/ClientFactory.java
@@ -16,20 +16,6 @@ package edu.oswego.csc480_hci521_2013.client;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.place.shared.PlaceController;
 
-import edu.oswego.csc480_hci521_2013.client.presenters.DataPanelPresenter;
-import edu.oswego.csc480_hci521_2013.client.services.H2OServiceAsync;
-import edu.oswego.csc480_hci521_2013.client.ui.MenuView;
-import edu.oswego.csc480_hci521_2013.client.ui.PanelView;
-
 public interface ClientFactory {
 
-    EventBus getEventBus();
-
-    PlaceController getPlaceController();
-
-    MenuView getMainView();
-    PanelView getDoublePanelView();
-    DataPanelPresenter.View getDataPanelView();
-
-    H2OServiceAsync getH2OService();
 }
