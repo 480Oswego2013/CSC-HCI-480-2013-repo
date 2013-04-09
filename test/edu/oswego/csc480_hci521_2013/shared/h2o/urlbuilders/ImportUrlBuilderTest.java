5b72530d36b7d7a81e55c71b00958bdc4576467b
480Oswego2013 <jwhite8@oswego.edu>
2013-04-09 12:53:02 -0400

    import test added

---
+++ test/edu/oswego/csc480_hci521_2013/shared/h2o/urlbuilders/ImportUrlBuilderTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2013 parris.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.oswego.csc480_hci521_2013.shared.h2o.urlbuilders;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ImportUrlBuilderTest {
+
+	//make the jar to run my test
+	ImportUrlBuilder myJar; 
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		//lets me use my jar without initializing in every test
+		myJar = new ImportUrlBuilder();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/* a test to see if the set key method is working*/
+	@Test
+	public void testIfNull() {
+		String myString = "THUNDER CATS!!!!";
+		myJar.setKey(myString);
+		assertNotNull("key is not Null",myJar);		
+	}
+	
+	/* check that the values are the same */
+	@Test 
+	public void checkKeyValue() throws Exception {
+		String myString = "THUNDER CATS!!!!";
+	ImportUrlBuilder keyValue = myJar.setKey(myString);
+	ImportUrlBuilder otherValue = myJar.setKey(myString);
+	
+	// return type is of import builder
+	assertEquals("the key value is equal",otherValue,keyValue);	
+	}
+
+}
