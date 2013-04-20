0e35b05905274589e50f07b55eb18bfa94525a08
Kevin Winahradsky <kevin.winahradsky@gmail.com>
2013-04-20 15:03:48 -0400

    added license headers

--- test/edu/oswego/csc480_hci521_2013/shared/h2o/urlbuilders/RedirectRequestFactoryTest.java
+++ test/edu/oswego/csc480_hci521_2013/shared/h2o/urlbuilders/RedirectRequestFactoryTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 State University of New York at Oswego
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
 package edu.oswego.csc480_hci521_2013.shared.h2o.urlbuilders;
 
 import org.junit.Before;
@@ -6,45 +21,45 @@ import edu.oswego.csc480_hci521_2013.shared.h2o.json.ResponseStatus;
 
 public class RedirectRequestFactoryTest {
 
-// jamie made this test 
-	RedirectRequestFactory myRRF; 
-	ResponseStatus myStatus; 
-	H2ORequest myH2oResult; 
+// jamie made this test
+	RedirectRequestFactory myRRF;
+	ResponseStatus myStatus;
+	H2ORequest myH2oResult;
 
-	
+
 	@Before
 	public void setUp() throws Exception {
 		//lets me use my jar without initializing in every test
 		myRRF = new RedirectRequestFactory();
                 myStatus = new ResponseStatus();
 	}
-	
+
 	/*
-	 * check if the redirect test is being called 
+	 * check if the redirect test is being called
 	 */
 	@Test
 	public void testisRedirect() throws Exception {
 		String result;
 		myStatus.setStatus("redirect");
-		 
+
               result = myStatus.getStatus();
 		//if the status is true
 		//a redirect was requested and message was sent
 		if(result.equals("redirect")) {
-			
+
 			System.out.println("test isRedirect Passed");
 		}
 		else
-			
-			// no message a was sent 
+
+			// no message a was sent
 			System.out.println("test isRedirect failed");
 	}// end method
-	
+
 	/*
-	 * this test will check the exception 
+	 * this test will check the exception
 	 * is working at then end of the argument
 	 */
-	@Test 
+	@Test
 	public void testFirstException(){
 		//set the boolean to false
 		myStatus.setStatus("error");
@@ -52,43 +67,43 @@ public class RedirectRequestFactoryTest {
 			RedirectRequestFactory.getRequest(myStatus);
 		}
 		catch(IllegalArgumentException e){
-			
+
 			System.out.println("caught expected error");
 		}
-	}// end method 
-	
+	}// end method
+
 	/*
 	 * this test will check if the last catch exception is working
 	 */
-	@Test 
+	@Test
 	public void testLastException(){
 		//set the redirect to fail
 		myStatus.setStatus("redirect");
                 myStatus.setRedirectRequest("error");
-		
+
 		try{
 			RedirectRequestFactory.getRequest(myStatus);
-                        
+
 		}
 		catch(IllegalArgumentException e){
-			
+
 			System.out.println("caught expected error");
 		}
 	}
-	
-	/* 
-	 * this test will test the importurl name 
+
+	/*
+	 * this test will test the importurl name
 	 */
 	@Test
 	public void testResquestImportURL(){
-            
+
                 // excpected value
                 String myValue = "ImportUrl";
-                    
+
                 // setting the status object
 		myStatus.setStatus("redirect");
                 myStatus.setRedirectRequest("ImportUrl");
-                
+
                 //checking the methods of the status object and redirect factory
                 if(myStatus.getRedirectRequest().equals(myValue))
                 {
@@ -97,20 +112,20 @@ public class RedirectRequestFactoryTest {
                 else
                     System.out.println("the ImportUrl name is wrong");
 	}// end method
-        
-        /* 
-	 * this test will test the InspectBuilder name 
+
+        /*
+	 * this test will test the InspectBuilder name
 	 */
 	@Test
 	public void testResquestInspectBuilder(){
-            
+
                 // excpected value
                 String myValue = "Inspect";
-                    
+
                 // setting the status object
 		myStatus.setStatus("redirect");
                 myStatus.setRedirectRequest(myValue);
-                
+
                 //checking the methods of the status object and redirect factory
                 if(myStatus.getRedirectRequest().equals(myValue))
                 {
@@ -119,20 +134,20 @@ public class RedirectRequestFactoryTest {
                 else
                     System.out.println("the InspectBuilder name is wrong");
 	}// end method
-        
-         /* 
-	 * this test will test the ParseBuilder name 
+
+         /*
+	 * this test will test the ParseBuilder name
 	 */
 	@Test
 	public void testResquestParseBuilder(){
-            
+
                 // excpected value
                 String myValue = "Parse";
-                    
+
                 // setting the status object
 		myStatus.setStatus("redirect");
                 myStatus.setRedirectRequest(myValue);
-                
+
                 //checking the methods of the status object and redirect factory
                 if(myStatus.getRedirectRequest().equals(myValue))
                 {
@@ -141,20 +156,20 @@ public class RedirectRequestFactoryTest {
                 else
                     System.out.println("the ParseBuilder name is wrong");
 	}// end method
-        
-        /* 
-	 * this test will test the ProgressBuilder name 
+
+        /*
+	 * this test will test the ProgressBuilder name
 	 */
 	@Test
 	public void testResquestProgressBuilder(){
-            
+
                 // excpected value
                 String myValue = "Progress";
-                    
+
                 // setting the status object
 		myStatus.setStatus("redirect");
                 myStatus.setRedirectRequest(myValue);
-                
+
                 //checking the methods of the status object and redirect factory
                 if(myStatus.getRedirectRequest().equals(myValue))
                 {
@@ -163,20 +178,20 @@ public class RedirectRequestFactoryTest {
                 else
                     System.out.println("the ProgressBuilder name is wrong");
 	}// end method
-        
-        /* 
-	 * this test will test the RFBuilder name 
+
+        /*
+	 * this test will test the RFBuilder name
 	 */
 	@Test
 	public void testResquestRFBuilder(){
-            
+
                 // excpected value
                 String myValue = "RF";
-                    
+
                 // setting the status object
 		myStatus.setStatus("redirect");
                 myStatus.setRedirectRequest(myValue);
-                
+
                 //checking the methods of the status object and redirect factory
                 if(myStatus.getRedirectRequest().equals(myValue))
                 {
@@ -185,20 +200,20 @@ public class RedirectRequestFactoryTest {
                 else
                     System.out.println("the RFBuilder name is wrong");
 	}// end method
-        
-        /* 
-	 * this test will test the InspectBuilder name 
+
+        /*
+	 * this test will test the InspectBuilder name
 	 */
 	@Test
 	public void testResquestRFTreeViewBuilder(){
-            
+
                 // excpected value
                 String myValue = "RFTreeView";
-                    
+
                 // setting the status object
 		myStatus.setStatus("redirect");
                 myStatus.setRedirectRequest(myValue);
-                
+
                 //checking the methods of the status object and redirect factory
                 if(myStatus.getRedirectRequest().equals(myValue))
                 {
@@ -207,20 +222,20 @@ public class RedirectRequestFactoryTest {
                 else
                     System.out.println("the RFTreeViewBuilder name is wrong");
 	}// end method
-        
-                                         /* 
-	 * this test will test the InspectBuilder name 
+
+                                         /*
+	 * this test will test the InspectBuilder name
 	 */
 	@Test
 	public void testResquestStoreViewBuilder(){
-            
+
                 // excpected value
                 String myValue = "StoreView";
-                    
+
                 // setting the status object
 		myStatus.setStatus("redirect");
                 myStatus.setRedirectRequest(myValue);
-                
+
                 //checking the methods of the status object and redirect factory
                 if(myStatus.getRedirectRequest().equals(myValue))
                 {
@@ -229,6 +244,6 @@ public class RedirectRequestFactoryTest {
                 else
                     System.out.println("the StoreViewBuilder name is wrong");
 	}// end method
-        
+
 }
 
