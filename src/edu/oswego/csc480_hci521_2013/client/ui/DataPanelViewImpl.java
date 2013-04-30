1cfd09edea42150ff070d03a4cfba6bf53c1c146
Nicholas Ibarluzea <nickibar@gmail.com>
2013-04-30 03:18:01 -0400

    Added basic DataGrid paging, issue #10.
    This is very simplistic paging and a more advanced and potentially
    useful custom paging widget is under development.

--- src/edu/oswego/csc480_hci521_2013/client/ui/DataPanelViewImpl.java
+++ src/edu/oswego/csc480_hci521_2013/client/ui/DataPanelViewImpl.java
@@ -24,13 +24,16 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.cellview.client.DataGrid;
 import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy;
+import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.cellview.client.SimplePager.TextLocation;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
 import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
 
 import edu.oswego.csc480_hci521_2013.client.activity.DoublePanelActivity;
 import edu.oswego.csc480_hci521_2013.client.presenters.DataPanelPresenter;
@@ -52,6 +55,7 @@ public class DataPanelViewImpl extends Composite implements DataPanelView {
     }
     private static Binder uiBinder = GWT.create(Binder.class);
     private DataPanelPresenter presenter;
+    private ListDataProvider<Map<String, String>> dataProvider = new ListDataProvider<Map<String, String>>();
     
     @UiField
     PushButton popin;
@@ -67,6 +71,8 @@ public class DataPanelViewImpl extends Composite implements DataPanelView {
     MenuItem trees;
     @UiField
     DataGrid<Map<String, String>> dataTable;
+    @UiField
+    SimplePager pager;
 
     @UiFactory
     DataGrid<Map<String, String>> makeDataGrid() {
@@ -79,8 +85,15 @@ public class DataPanelViewImpl extends Composite implements DataPanelView {
     }
     
     public DataPanelViewImpl(boolean showPopinButton) {
+    	SimplePager.Resources pagerResources = GWT.create(SimplePager.Resources.class);
+        pager = new SimplePager(TextLocation.CENTER, pagerResources, false, 0, true);
+    	
     	initWidget(uiBinder.createAndBindUi(this));
         dataTable.setKeyboardSelectionPolicy(HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.DISABLED);
+        dataProvider.addDataDisplay(dataTable);
+        pager.setDisplay(dataTable);
+//        pager.setRangeLimited(false);
+        
         popinPanel.setVisible(showPopinButton);
     }
 
@@ -128,7 +141,8 @@ public class DataPanelViewImpl extends Composite implements DataPanelView {
 
     @Override
     public void setData(List<Map<String, String>> data) {
-        dataTable.setRowData(data);
+//        dataTable.setRowData(data);
+    	dataProvider.setList(data);
     }
     
     @Override
